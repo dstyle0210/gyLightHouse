f8c2c056627042d6a1310339cb35d5c384d56714
dstyle0210 <dstyle0210@gmail.com>
2024-07-16 11:11:04 +0900

    최최종

--- send.js
+++ send.js
@@ -20,16 +20,15 @@ async function sendResult(){
     fs.mkdirSync("./report",{recursive:true});
     const networkOrigin = fs.readFileSync("./report/network.json");
     const networkData = JSON.parse(networkOrigin);
-    const networkData2 = {min:networkData.min * 0.2,max:networkData.max * 0.2,arg:networkData.arg * 0.2};
+    const networkData2 = {min:Math.floor(networkData.min * 0.2),max:Math.floor(networkData.max * 0.2),arg:Math.floor(networkData.arg * 0.2)};
 
     console.log(networkData2);
 
-    /*
+    
     const bot = new TelegramBot(token, {polling: false});
-    const result = Object.assign({FCP,SI,LCP,TBT},networkData);
-    const message = `FCP : ${FCP*0.9}`
+    const result = Object.assign({FCP,SI,LCP,TBT},networkData2);
     bot.sendMessage(chatId, `[TEST] ${JSON.stringify(result)}`);
-    */
+    
 }
 sendResult();
 // import TelegramBot from 'node-telegram-bot-api';
